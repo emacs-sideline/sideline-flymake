86637b258fcf7516b9090bcd823069430abedb9d
Jen-Chieh <jcs090218@gmail.com>
2022-06-29 15:48:30 +0800

    docs: update

--- sideline-flymake.el
+++ sideline-flymake.el
@@ -26,7 +26,15 @@
 
 ;;; Commentary:
 ;;
-;; Show flymake errors with sideline.
+;; This package allows display flymake errors with sideline.
+;;
+;; 1) Add sideline-flymake to sideline backends list,
+;;
+;;   (setq sideline-backends-right '(sideline-flymake))
+;;
+;; 2) Then enable sideline-mode,
+;;
+;;   M-x sideline-mode
 ;;
 
 ;;; Code:
