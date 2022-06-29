bd1658585d86acb3a28d7c1a8325f7862500bc96
Jen-Chieh <jcs090218@gmail.com>
2022-06-29 15:50:17 +0800

    Bump version

--- sideline-flymake.el
+++ sideline-flymake.el
@@ -5,7 +5,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-sideline/sideline-flymake
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "27.1") (sideline "0.1.0"))
 ;; Keywords: convenience flymake
 
