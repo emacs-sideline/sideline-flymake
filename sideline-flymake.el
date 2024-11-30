096b2fecfdb5a2192541e6099f761dde22b53aec
Jen-Chieh Shen <jcs090218@gmail.com>
2024-11-30 19:19:15 +0800

    feat: Drop Emacs 27.x support

--- sideline-flymake.el
+++ sideline-flymake.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-sideline/sideline-flymake
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (sideline "0.1.0"))
+;; Package-Requires: ((emacs "28.1") (sideline "0.1.0"))
 ;; Keywords: convenience flymake
 
 ;; This file is not part of GNU Emacs.
